15ca4e335c1683dda7639c668e8b1b416dfc7f68
Clement Auvray <cauvray@student.42lehavre.fr>
2024-07-08 17:48:17 +0200

    norm

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -6,13 +6,13 @@
 /*   By: cauvray <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 15:57:58 by cauvray           #+#    #+#             */
-/*   Updated: 2024/07/08 17:29:32 by cauvray          ###   ########.fr       */
+/*   Updated: 2024/07/08 17:46:59 by cauvray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strstr(char *str, char *to_find)
 {
-	char 		*finded;
+	char			*finded;
 	unsigned int	i;
 	unsigned int	j;
 
@@ -25,7 +25,6 @@ char	*ft_strstr(char *str, char *to_find)
 		{
 			if (!finded)
 				finded = &str[i];
-
 			if (to_find[j] == '\0')
 				return (finded);
 			j++;
