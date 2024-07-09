fa9db7ec629bbbb912d9d91429acc8cabf9e3809
Clement Auvray <cauvray@student.42lehavre.fr>
2024-07-09 18:37:12 +0200

    c04

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: cauvray <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 11:30:26 by cauvray           #+#    #+#             */
-/*   Updated: 2024/07/09 12:00:39 by cauvray          ###   ########.fr       */
+/*   Updated: 2024/07/09 17:29:38 by cauvray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,29 +16,39 @@ int	ft_atoi(char *str)
 {
 	unsigned int	i;
 	int	nb;
-	bool	is_negative;
+	int	is_negative;
 
 	i = 0;
 	nb = 0;
-	is_negative = false;
-	while (str[i] == " ")
+	is_negative = 0;
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
 		i++;
-	if (str[i] != "+" && str[i] != "-")
-		return 0;
-	while(str[i] == "-" || str[i] == "+")
+	if (str[i] != '+' && str[i] != '-')
+		return -8;
+	while(str[i] == '-' || str[i] == '+')
 	{
-		if (str[i] == "-")
-			is_negative = !is_negative;
+		if (str[i] == '-')
+		{
+			if (is_negative == 1)
+				is_negative = 0;
+			else
+				is_negative = 1;
+		}
 		i++;
 	}
 	if (str[i] < '0' || str[i] > '9')
-		return 0;
+		return -4;
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		nb = nb * 10 + str[i];
+		nb = (nb * 10) + str[i];
 		i++;
 	}
-	if (is_negative)
+	if (is_negative == 1)
 		nb *= -1;
 	return (nb);
+}
+
+int main(int argsc, char *args[])
+{
+	printf("%d", ft_atoi("   +-+--4564"));
 }
