c498df500bdf109877794be7734e4c42cd4dfc79
Clement Auvray <cauvray@student.42lehavre.fr>
2024-07-09 15:54:10 +0200

    c02 ex10'

--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -6,21 +6,33 @@
 /*   By: cauvray <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 22:09:27 by cauvray           #+#    #+#             */
-/*   Updated: 2024/07/09 13:36:54 by cauvray          ###   ########.fr       */
+/*   Updated: 2024/07/09 15:53:06 by cauvray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+unsigned int	ft_strlen(char *str)
 {
 	unsigned int	i;
 
 	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	src_len;
+
+	i = 0;
+	src_len = ft_strlen(src);
 	size -= 1;
-	while (src[i] && (i < size))
+	while (src[i])
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
-	return (i);
+	return (src_len);
 }
