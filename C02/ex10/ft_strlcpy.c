1a712776c77dc485e6535139f61981e17913e9f7
Clement Auvray <cauvray@student.42lehavre.fr>
2024-07-09 13:39:08 +0200

    fix c02 ex10

--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -6,19 +6,9 @@
 /*   By: cauvray <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 22:09:27 by cauvray           #+#    #+#             */
-/*   Updated: 2024/07/09 13:07:18 by cauvray          ###   ########.fr       */
+/*   Updated: 2024/07/09 13:36:54 by cauvray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-unsigned int	ft_strlen(char *str)
-{
-	unsigned int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
