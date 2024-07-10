51c6bb49257353b12a8a8d61862ec2b03a633ad0
Clement Auvray <clem29auv@gmail.com>
2024-07-10 21:50:54 +0200

    C06 ex1-2-3 ez

---
+++ C06/ex00/ft_print_program_name.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cauvray <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/10 21:22:46 by cauvray           #+#    #+#             */
+/*   Updated: 2024/07/10 21:39:23 by cauvray          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	(void) argc;
+	ft_putstr(argv[0]);
+	ft_putchar('\n');
+	return (0);
+}
