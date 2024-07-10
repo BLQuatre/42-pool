408485ae7f1e4eaa2093ad26fb425328a360423a
Clement Auvray <cauvray@student.42lehavre.fr>
2024-07-10 14:45:33 +0200

    fix atoi rm a.out

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,49 +6,41 @@
 /*   By: cauvray <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 11:30:26 by cauvray           #+#    #+#             */
-/*   Updated: 2024/07/09 17:29:38 by cauvray          ###   ########.fr       */
+/*   Updated: 2024/07/10 14:44:50 by cauvray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+int	ft_isspace(char c)
+{
+	if (c == ' ' || c == '\n' || c == '\v'
+		|| c == '\f' || c == '\r' || c == '\t')
+		return (1);
+	return (0);
+}
 
 int	ft_atoi(char *str)
 {
-	unsigned int	i;
 	int	nb;
-	int	is_negative;
+	int	negative;
+	int	i;
 
 	i = 0;
 	nb = 0;
-	is_negative = 0;
-	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+	negative = 0;
+	while (ft_isspace(str[i]) == 1)
 		i++;
-	if (str[i] != '+' && str[i] != '-')
-		return -8;
-	while(str[i] == '-' || str[i] == '+')
+	while (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
-		{
-			if (is_negative == 1)
-				is_negative = 0;
-			else
-				is_negative = 1;
-		}
+			negative += 1;
 		i++;
 	}
-	if (str[i] < '0' || str[i] > '9')
-		return -4;
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		nb = (nb * 10) + str[i];
+		nb = (nb * 10) + (str[i] - '0');
 		i++;
 	}
-	if (is_negative == 1)
+	if (negative % 2 != 0)
 		nb *= -1;
 	return (nb);
-}
-
-int main(int argsc, char *args[])
-{
-	printf("%d", ft_atoi("   +-+--4564"));
 }
