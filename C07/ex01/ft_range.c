43d2949fa072fb22dc2096ef40d8515189525e76
Clement Auvray <clem29auv@gmail.com>
2024-07-12 00:22:39 +0200

    c07 ex1

--- C07/ex01/ft_range.c
+++ C07/ex01/ft_range.c
@@ -6,34 +6,31 @@
 /*   By: cauvray <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 15:57:31 by cauvray           #+#    #+#             */
-/*   Updated: 2024/07/11 18:38:23 by cauvray          ###   ########.fr       */
+/*   Updated: 2024/07/11 23:59:14 by cauvray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 int	ft_range_len(int i1, int i2)
 {
-	while (i1 < i2)
-		i1++;
-	return (i1);
-}	
+	int	difference;
+
+	difference = i1 - i2;
+	if (difference < 0)
+		difference = -difference;
+	return (difference);
+}
 
 int	*ft_range(int min, int max)
 {
-	int		tab_len;
-	int	*tab;
+	int	tab_len;
 	int	i;
-
+	int	*tab;
 
 	if (min >= max)
 		return (NULL);
-
-
 	tab_len = ft_range_len(min, max);
-	printf("Len: %d\n", tab_len);
 	tab = (int *) malloc(tab_len);
 	i = 0;
 	while (i < tab_len)
@@ -43,17 +40,3 @@ int	*ft_range(int min, int max)
 	}
 	return (tab);
 }
-
-
-int main()
-{
-	int *tab = ft_range(-10, 20);
-	int i = 0;
-
-	while (tab[i])
-	{
-		printf("%d ", tab[i]);
-		i++;
-	}
-}
-
