ceb1adcffbda44742603dfb79b650cecbd784a17
Clement Auvray <cauvray@student.42lehavre.fr>
2024-07-15 14:01:47 +0200

    start c08

---
+++ C08/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cauvray <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/15 12:00:29 by cauvray           #+#    #+#             */
+/*   Updated: 2024/07/15 13:19:20 by cauvray          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cauvray <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/15 12:45:56 by cauvray           #+#    #+#             */
+/*   Updated: 2024/07/15 13:36:26 by cauvray          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+void	ft_putstr(char *str);
+
+# ifndef TRUE
+#  define TRUE 1
+# endif
+# ifndef FALSE
+#  define FALSE 0
+# endif
+
+typedef int	t_bool;
+
+t_bool	ft_is_even(int bnr);
+
+# ifndef EVEN
+#  define EVEN(nbr) (nbr % 2 == 0)
+# endif
+
+# ifndef EVEN_MSG
+#  define EVEN_MSG "I have an even number of arguments.\n"
+# endif
+# ifndef ODD_MSG
+#  define ODD_MSG "I have an odd number of arguments.\n"
+# endif
+# ifndef SUCCESS
+#  define SUCCESS 0
+# endif
+
+#endif
