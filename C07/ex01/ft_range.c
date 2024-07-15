e4b026183eae3abaf5ac6e1a0d85ff1286b098f9
Clement Auvray <cauvray@student.42lehavre.fr>
2024-07-15 10:59:54 +0200

    range

--- C07/ex01/ft_range.c
+++ C07/ex01/ft_range.c
@@ -6,21 +6,11 @@
 /*   By: cauvray <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 15:57:31 by cauvray           #+#    #+#             */
-/*   Updated: 2024/07/11 23:59:14 by cauvray          ###   ########.fr       */
+/*   Updated: 2024/07/15 10:58:58 by cauvray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-
-int	ft_range_len(int i1, int i2)
-{
-	int	difference;
-
-	difference = i1 - i2;
-	if (difference < 0)
-		difference = -difference;
-	return (difference);
-}
 
 int	*ft_range(int min, int max)
 {
@@ -30,7 +20,7 @@ int	*ft_range(int min, int max)
 
 	if (min >= max)
 		return (NULL);
-	tab_len = ft_range_len(min, max);
+	tab_len = max - min;
 	tab = (int *) malloc(tab_len);
 	i = 0;
 	while (i < tab_len)
--- C07/ex02/ft_ultimate_range.c
+++ C07/ex02/ft_ultimate_range.c
@@ -6,39 +6,31 @@
 /*   By: cauvray <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 15:57:31 by cauvray           #+#    #+#             */
-/*   Updated: 2024/07/12 12:28:41 by cauvray          ###   ########.fr       */
+/*   Updated: 2024/07/15 10:51:31 by cauvray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int	ft_range_len(int i1, int i2)
-{
-	int	difference;
-
-	difference = i1 - i2;
-	if (difference < 0)
-		difference = -difference;
-	return (difference);
-}
-
-int	*ft_ultimate_range(int **range, int min, int max)
+int	ft_ultimate_range(int **range, int min, int max)
 {
 	int	tab_len;
 	int	i;
-	int	*tab;
 
-	if (min >= max) {
-		**range = NULL;
+	if (min >= max)
+	{
+		*range = NULL;
 		return (0);
 	}
-	tab_len = ft_range_len(min, max);
-	range = (int *) malloc(tab_len);
+	tab_len = max - min;
+	*range = (int *) malloc(tab_len * 4);
+	if (*range == NULL)
+		return (-1);
 	i = 0;
 	while (i < tab_len)
 	{
-		tab[i] = min + i;
+		(*range)[i] = min + i;
 		i++;
 	}
-	return (tab);
+	return (tab_len);
 }
